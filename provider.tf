265e87076843a131ad35a7c592ac8eba02fa93f6
ShubhamSinghal1811 <83664477+ShubhamSinghal1811@users.noreply.github.com>
2022-11-09 11:32:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA6JWWL57AW3JMTFVQ"
-  secret_key = "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"
+  access_key = "AKIA6JWWL57AW3JMTFVQA"
+  secret_key = "lmYw8clz2mFgk2ZtJivBo+4Vlpb1ivZu7d+sT3fiA"
   
 }
